bf9e06bbd74e920b18d41363010a82e1e82a6c19
Prodip K Saha <prodip.kumar@outlook.com>
2017-11-13 00:35:46 -0600

    Get data from API

--- src/app/address-book.service.ts
+++ src/app/address-book.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Address } from './address';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const Addresses = [
   { name: "John", phone: "214-555-1212", city: "Dallas" },
@@ -11,13 +12,32 @@ const Addresses = [
   { name: 'Ravi', phone: '214-456-1212', city: 'Grapevine' }
 ];
 
+var Addresses2: Address[];
+
 @Injectable()
 export class AddressBookService {
-  getAddresses() { return Addresses; }
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAddresses() {
+    let headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
+    headers = headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    headers = headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    this.http.get<Address[]>("http://api.aspnet4you.com/api/addressbook/GetAllAddresses", { headers: headers })
+      .subscribe(
+      data => { console.log(data); Addresses2 = data },
+      error => { console.log('Something went wrong, can not get the data!');}
+      );
+      
+
+    return Addresses2;
+  }
 
   getAddress(name: string)
   {
-    let findAddress = Addresses.filter(a => a.name === name);
+    let findAddress = Addresses2.filter(a => a.name === name);
 
     if (findAddress.length > 0) {
       return Addresses[0];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule }       from '@angular/platform-browser';
 import { FormsModule }         from '@angular/forms';
-import { NgModule }     from '@angular/core';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroListComponent }   from './hero-list.component';
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule
   ],
