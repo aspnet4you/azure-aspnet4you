c25830207d9c0ab4569cb1da4389b88d6585d683
Prodip K Saha <prodip.kumar@outlook.com>
2017-12-25 10:10:15 -0600

    Enable SSL. Configure the cookie to be secure. SSL is already turned on in Azure. All traffic in prod must travel through HTTPS transport.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiDomain: "http://api.aspnet4you.com",
-  returnUrl: "http://azure.aspnet4you.com/oauth",
+  apiDomain: "https://api.aspnet4you.com",
+  returnUrl: "https://azure.aspnet4you.com/oauth",
   cookieDomain: "azure.aspnet4you.com",
   //apiDomain: "http://api-local.aspnet4you.com",
   //returnUrl: "http://local.angular4.com/oauth",
   //cookieDomain: "local.angular4.com",
-  secureCookie: false
+  secureCookie: true
 };
